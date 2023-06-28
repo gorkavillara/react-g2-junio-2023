45b76ca3b1af148972577ade83bc14e0b854d926
gorkavillar <gorka@gorkavillar.com>
2023-06-28 14:05:37 +0200

    Tema 09 - Utilizando fetch y axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,62 @@
+import { useState } from "react"
+import axios from "axios"
 import "./App.scss"
-import { Link, Outlet, NavLink } from "react-router-dom"
+
+const baseUrl = "https://rickandmortyapi.com/api/character/"
+
+interface Personaje {
+  name: string
+  id: number
+  image: string
+}
 
 function App() {
+  const [id, setId] = useState<string>("")
+  const [personaje, setPersonaje] = useState<Personaje | null>(null)
+
+  const buscaIdFetch = async () => {
+    // hacer la búsqueda del id
+    const url = `${baseUrl}${id}`
+    const respuesta = await fetch(url)
+    console.log(respuesta)
+    const info = await respuesta.json()
+    console.log(info)
+  }
+  const buscaIdAxios = async () => {
+    // hacer la búsqueda del id
+    const url = `${baseUrl}${id}`
+    // try {
+      // const { data: pers } = await axios.get(url)
+      // console.log(pers)
+      // setPersonaje(pers)
+    // } catch (e) {
+      // window.alert("Error!")
+    // }
+    axios.get(url)
+      .then(res => setPersonaje(res.data))
+      .catch(error => console.error(error))
+      .finally(() => console.log("Llamada finalizada"))
+  }
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/contacto">Contacto</NavLink>
-          </li>
-          <li>
-            <NavLink to="/alumnos" >Alumnos</NavLink>
-          </li>
-          <li>
-            <NavLink to="/formulario">Formulario</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-          <li>
-            <a href="https://google.com">Google</a>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
+      <div>
+        <h3>Búsqueda de Rick y Morty</h3>
+        <input
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          placeholder="Introduce el id a buscar"
+        />
+        <button onClick={buscaIdFetch}>Buscar</button>
+        <button onClick={buscaIdAxios}>Axios</button>
+      </div>
+      {personaje && (
+        <>
+          <p>
+            {personaje.id}. {personaje.name}
+          </p>
+          <img src={personaje.image} />
+        </>
+      )}
     </>
   )
 }

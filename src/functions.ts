e593e7dbf400def32c59c590ca452d76455bd401
gorkavillar <gorka@gorkavillar.com>
2023-07-20 17:03:06 +0200

    Tema 15 - Últimos cambios

--- src/functions.ts
+++ src/functions.ts
@@ -2,15 +2,15 @@
  * La función suma devuelve la suma aritmética de dos parámetros
  * @param a El primer parámetro a sumar
  * @param b El segundo parámetro a sumar
- * @returns La suma aritmética de a y b
+ * @returns {number} La suma aritmética de a y b
  */
-const suma = (a: number, b: number) => a + b
+const suma = (a: number, b: number): number => a + b
 
 /**
  * Obtiene el número de letras del nombre que le pasamos por parámetro
  * @param nombre El nombre del cual queremos obtener el número de letras
- * @returns El número de letras del nombre
+ * @returns {number} El número de letras del nombre
  */
-const obtenNumeroLetras = (nombre: string) => nombre.length
+const obtenNumeroLetras = (nombre: string): number => nombre.length
 
 export { suma, obtenNumeroLetras }
